bd94ef3529affa67312124900cb619f8ce5c8ff2
darkroastjava <danieljaegerjordi@gmail.com>
2013-07-03 10:05:36 +0200

    Separated DataSource from BindingSource

--- ko.collectionBinding.js
+++ ko.collectionBinding.js
@@ -3,34 +3,57 @@ if (!ko) {
 }
 
 ko.collectionBinding = (function (ko) {
-    var BindingSource = function (data) {
-        this.rows = ko.observableArray(data);
-        this.currentlySortedBy = ko.observable(null);
+    var DataSource = function (data) {
+        this.items = data;
+        this.subscribersForAdd = [];
     },
+        BindingSource = function (dataSource) {
+            var self = this;
+            self.dataSource = dataSource;
+            self.items = ko.observableArray(dataSource.getItems());
+            dataSource.subscribeForAdd(function(newItem){
+                var currentlySortedBy = self.currentlySortedBy(),
+                    sorter;
+                
+                self.items.push(newItem);
+                if (currentlySortedBy) {
+                    sorter = self.sorters[currentlySortedBy];
+                    
+                    if (sorter.state() === 'desc') {
+                        sorter.state('asc');
+                    } else {
+                        sorter.state(null);
+                    }
+                    
+                    sorter.sort();
+                }
+            });
+            self.currentlySortedBy = ko.observable(null);
+            self.sorters = {};
+        },
         comparerForProperty = function (property) {
             return function (left, right) {
                 return left[property] == right[property] ? 0 : left[property] < right[property] ? -1 : 1;
             };
         },
-        sorters = {},
-        Sorter = function (property, dataGrid) {
+        Sorter = function (property, bindingSource, sorters) {
             var self = this,
                 state = ko.observable(false),
-                compare = dataGrid.comparerForProperty(property),
+                compare = bindingSource.comparerForProperty(property),
                 sort = function () {
-                    if (dataGrid.currentlySortedBy() && dataGrid.currentlySortedBy() != property) {
-                        sorters[dataGrid.currentlySortedBy()].state(null);
+                    if (bindingSource.currentlySortedBy() && bindingSource.currentlySortedBy() != property) {
+                        sorters[bindingSource.currentlySortedBy()].state(null);
                     }
-                    dataGrid.currentlySortedBy(property);
-    
+                    bindingSource.currentlySortedBy(property);
+
                     if (state() === 'asc') {
                         state('desc');
-                        dataGrid.rows.sort(function (left, right) {
+                        bindingSource.items.sort(function (left, right) {
                             return compare(right, left);
                         });
                     } else {
                         state('asc');
-                        dataGrid.rows.sort(compare);
+                        bindingSource.items.sort(compare);
                     }
                 };
     
@@ -47,19 +70,41 @@ ko.collectionBinding = (function (ko) {
             self.sort = sort;
         };
 
+    DataSource.prototype.notifyAdd = function (newItem) {
+        var i, n, subscribers = this.subscribersForAdd;
+        
+        for (i = 0, n = subscribers.length; i < n; i++) {
+            subscribers[i](newItem);
+        }
+    };
+    
+    DataSource.prototype.addItem = function (newItem) {
+        this.items.push(newItem);
+        this.notifyAdd(newItem);
+    };
+    
+    DataSource.prototype.subscribeForAdd = function (callback) {
+        this.subscribersForAdd.push(callback);
+    };
+    
+    DataSource.prototype.getItems = function () {
+        return this.items.slice(0); // clone
+    };
+    
     BindingSource.prototype.comparerForProperty = comparerForProperty;
 
     BindingSource.prototype.sorter = function (property) {
-        var sorter = sorters[property];
+        var sorter = this.sorters[property];
 
         if (!sorter) {
-            sorter = sorters[property] = new Sorter(property, this);
+            sorter = this.sorters[property] = new Sorter(property, this, this.sorters);
         }
 
         return sorter;
     };
 
     return {
+        DataSource: DataSource,
         BindingSource: BindingSource
     };
 }(ko));
